5260f82d2024c7278505d5794d856bb21c2fc9c0
pazuhiko <45529381+pazuhiko@users.noreply.github.com>
2021-07-12 04:02:47 +0900

    Update cameraif.ino

--- cameraif.ino
+++ cameraif.ino
@@ -2,12 +2,15 @@
 
 
 
+const int D4 = 4;
+const float freq = 120;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(D4, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  tone(D4, freq);
 }
